a5f7e12ce22fb6274082cab7528430963d2451ad
Kalindu Himasara <167981628+kalinduhimasara07@users.noreply.github.com>
2026-06-16 07:56:55 +0530

    comment somecodes

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,12 +1,12 @@
-import { Search } from 'lucide-react'
-import React from 'react'
+// import { Search } from 'lucide-react'
+// import React from 'react'
 
-const SearchBar = () => {
-  return (
-    <div>
-      <Search className='w-5 h-5 hover:text-shop_light_green hoverEffect' />
-    </div>
-  )
-}
+// const SearchBar = () => {
+//   return (
+//     <div>
+//       <Search className='w-5 h-5 hover:text-shop_light_green hoverEffect' />
+//     </div>
+//   )
+// }
 
-export default SearchBar
+// export default SearchBar
